361d1ae64fad7dfb37afbe09e149abdbb25cddb6
Kevin Mills <kevin.mills@sifive.com>
2018-10-16 07:15:39 -0700

    Added code comments

--- bundles/ilg.gnumcueclipse.debug.gdbjtag.openocd/src/ilg/gnumcueclipse/debug/gdbjtag/openocd/dsf/SiFiveMemory.java
+++ bundles/ilg.gnumcueclipse.debug.gdbjtag.openocd/src/ilg/gnumcueclipse/debug/gdbjtag/openocd/dsf/SiFiveMemory.java
@@ -30,6 +30,9 @@ public class SiFiveMemory extends GDBMemory_7_6 {
     public void getMemory(IMemoryDMContext memoryDMC, IAddress address, long offset,
     		int wordSize, int wordCount, DataRequestMonitor<MemoryByte[]> drm)
 	{
+		/*
+		 * Original code from the base class, unchanged here
+		 */
         if (memoryDMC == null) {
             drm.setStatus(new Status(IStatus.ERROR, GdbPlugin.PLUGIN_ID, INTERNAL_ERROR, "Unknown context type", null)); //$NON-NLS-1$);
             drm.done();            
@@ -48,6 +51,13 @@ public class SiFiveMemory extends GDBMemory_7_6 {
     		return;
     	}
 
+		/*
+		 * End of original code from the base class
+		 */
+
+    	/*
+    	 * New code that simply checks the current runstate and bails if the target is running.
+    	 */
     	boolean proceed = false;
     	IMIContainerDMContext containerCtx = DMContexts.getAncestorOfType(memoryDMC, IMIContainerDMContext.class);
 		if(containerCtx != null) {
@@ -68,11 +78,22 @@ public class SiFiveMemory extends GDBMemory_7_6 {
 			}
 		}
 		if (!proceed) {
+			/*
+			 * Don't report and error, the UI does not report this in a friendly way.  Returning a 'success' is handled with a better message in the UI.
+			 */
+			
     		//drm.setStatus(new Status(IStatus.CANCEL, GdbPlugin.PLUGIN_ID, IDsfStatusConstants.REQUEST_FAILED, "Cannot read memory while running", null)); //$NON-NLS-1$
+			
+			/*
+			 * Ensure the monitor is completed
+			 */
     		drm.done();
     		return;
 		}
     	
+		/*
+		 * Everything looks good at this point, go ahead and try to read memory from the target.
+		 */
     	getMemoryCache(memoryDMC).getMemory(memoryDMC, address.add(offset), wordSize, wordCount, drm);
 	}
 }
